c9c776ae252c075d03a773982e488aadea1fa739
Divyam Shah <divyamshah1234@gmail.com>
2025-09-01 23:21:22 +0530

    aerg

--- jirawala_tours_travels/jirawala_tours_travels/settings.py
+++ jirawala_tours_travels/jirawala_tours_travels/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-7c13h4l24!c&raz^t7+gykk_hbr+7mz8%@onorn9!lv6#v22#+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
